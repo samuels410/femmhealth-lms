c735b56bbda6b2d4dcbf9bae160f5f74435e47cd
Ubuntu <ubuntu@teach.femmhealth.org>
2017-03-23 16:31:21 +0000

    femm lms with branding

---
+++ public/branding/femm.js
@@ -0,0 +1,7 @@
+/* this file is not used at present */
+
+document.getElementById('login_forgot_password').innerHTML = 'Did you forget your password?';
+var footer = document.getElementById('footer');
+footer.innerHTML = 
+  '<span><a href="http://femmhealth.org"><img src="/branding/femm_logo_canvas_small.png" />&nbsp;&nbspFEMM Health</a></span>';
+
